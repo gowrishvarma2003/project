825097fd8b17ebf92e3d49e0333ab052ba577c52
gowrishvarma <gowrishvarma@gmail.com>
2023-11-26 18:11:04 +0530

    added order function

--- uropnew/lib/home/mycart.dart
+++ uropnew/lib/home/mycart.dart
@@ -1,8 +1,10 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import '../main.dart' as mainfile;
 import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:convert';
 
 class mycart extends StatefulWidget {
   @override
@@ -16,6 +18,16 @@ class Product {
   final String productName;
   final String image;
 
+    Map<String, dynamic> toJson() {
+    return {
+      'user': user,
+      'quantity': quantity,
+      'price': price,
+      'productName': productName,
+      'image': image,
+    };
+  }
+
   Product(
       {required this.user,
       required this.quantity,
@@ -24,8 +36,73 @@ class Product {
       required this.image});
 }
 
+
+
 class _mycartState extends State<mycart> {
   List<Product> products = [];
+
+Future<void> sendorders(List<Product> products, Map<String, dynamic> userDetails) async {
+  try {
+    // Convert the list of products to a list of JSON objects
+    List<Map<String, dynamic>> productsJson = products.map((product) => product.toJson()).toList();
+
+    // Add user-specific details to the payload
+    Map<String, dynamic> payload = {
+      'userDetails': userDetails,
+      'products': productsJson,
+    };
+
+    // Send the combined payload to the server
+    final response = await http.post(
+      Uri.parse('http://' + mainfile.ip + '/server/send_products'),
+      body: jsonEncode(payload),
+      headers: {'Content-Type': 'application/json'},
+    );
+
+    if (response.statusCode == 200) {
+      print('Products sent successfully');
+    } else {
+      throw Exception('Failed to send products');
+    }
+  } catch (e) {
+    print(e);
+  }
+}
+
+
+  Future<void> order() async {
+    try {
+      print("asdfg");
+      List<Product> fetchedProducts = await fetchProductsFromServer();
+      print(fetchedProducts[0].quantity);
+      setState(() {
+        products = fetchedProducts; // Update the products list
+        // print(products[0]);
+      });
+
+      var user = FirebaseAuth.instance.currentUser!.phoneNumber;
+      if (user != null) {
+        user = user.replaceFirst("+91", ""); // Modify user data as needed
+        print(user);
+      } else {
+        print('User data not available.');
+        return; // Return if user data is not available
+      }
+
+
+      Map<String, dynamic> userDetails = {
+        'user': user,
+    };
+
+      // Send the fetched products back to the server
+      await sendorders(products, userDetails);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+
+
 
   Future<List<Product>> fetchProductsFromServer() async {
     var user = FirebaseAuth.instance.currentUser!.phoneNumber;
@@ -86,6 +163,7 @@ class _mycartState extends State<mycart> {
           //   context,
           //   MaterialPageRoute(builder: (context) => sell_products()),
           // );
+          order();
         },
         child: Text('Add products'),
       ));
@@ -135,10 +213,11 @@ class _mycartState extends State<mycart> {
         body: Column(
       children: [
         Container(
-            height: 620,
-            child: home_products(),
+          height: 620,
+          child: home_products(),
         ),
-      Expanded(child: button())],
+        Expanded(child: button())
+      ],
     ));
   }
 }
